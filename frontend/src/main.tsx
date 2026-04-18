29dc5268ce3c6497204fe0ddc526706ee5690672
Dorna Raj Gyawali <dronarajgyawali@gmail.com>
2026-04-18 13:42:39 +0545

    removed strict mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
@@ -9,7 +8,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <BrowserRouter>
@@ -17,5 +15,4 @@ createRoot(document.getElementById("root")!).render(
         </BrowserRouter>
       </TooltipProvider>
     </QueryClientProvider>
-  </StrictMode>,
 );
